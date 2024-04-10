6b30e62ef6ddba3066950017cb7b22044a404ab5
RichardKobay <ricardo.roel110@gmail.com>
2024-04-09 22:56:43 -0600

    Read all sheets

--- app/src/main/java/org/example/Professor.java
+++ app/src/main/java/org/example/Professor.java
@@ -2,11 +2,29 @@ package org.example;
 
 public class Professor {
     private String name;
-    private int hours;
+    private int ITIHours;
+    private int IMHours;
+    private int ITMHours;
+    private int ISAHours;
+    private int LAyGEHours;
+    private int comercioHours;
 
     public Professor() {
         this.name = "";
-        this.hours = 0;
+        this.ITIHours = 0;
+        this.IMHours = 0;
+        this.ITMHours = 0;
+        this.ISAHours = 0;
+        this.LAyGEHours = 0;
+    }
+
+    public Professor(String name) {
+        this.name = name;
+        this.ITIHours = 0;
+        this.IMHours = 0;
+        this.ITMHours = 0;
+        this.ISAHours = 0;
+        this.LAyGEHours = 0;
     }
 
     public String getName() {
@@ -17,11 +35,51 @@ public class Professor {
         this.name = name;
     }
 
-    public int getHours() {
-        return hours;
+    public int getITIHours() {
+        return ITIHours;
     }
 
-    public void setHours(int hours) {
-        this.hours = hours;
+    public void setITIHours(int ITIHours) {
+        this.ITIHours = ITIHours;
+    }
+
+    public int getIMHours() {
+        return IMHours;
+    }
+
+    public void setIMHours(int IMHours) {
+        this.IMHours = IMHours;
+    }
+
+    public int getITMHours() {
+        return ITMHours;
+    }
+
+    public void setITMHours(int ITMHours) {
+        this.ITMHours = ITMHours;
+    }
+
+    public int getISAHours() {
+        return ISAHours;
+    }
+
+    public void setISAHours(int ISAHours) {
+        this.ISAHours = ISAHours;
+    }
+
+    public int getLAyGEHours() {
+        return LAyGEHours;
+    }
+
+    public void setLAyGEHours(int LAyGEHours) {
+        this.LAyGEHours = LAyGEHours;
+    }
+
+    public int getComercioHours() {
+        return comercioHours;
+    }
+
+    public void setComercioHours(int comercioHours) {
+        this.comercioHours = comercioHours;
     }
 }
--- app/src/main/java/org/example/SheetsAndJava.java
+++ app/src/main/java/org/example/SheetsAndJava.java
@@ -27,80 +27,87 @@ public class SheetsAndJava {
     private static final String SPREADSHEET_ID = "1pLgKaAENH1mYW3fAKpcGq8GSCt0V8epyuVdbBg2caqk";
 
     private static Credential authorize() throws IOException, GeneralSecurityException {
-        InputStream inputStream =  SheetsAndJava.class.getResourceAsStream("/credentials.json");
-        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(
-                GsonFactory.getDefaultInstance(), new InputStreamReader(Objects.requireNonNull(inputStream))
-        );
+        InputStream inputStream = SheetsAndJava.class.getResourceAsStream("/credentials.json");
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(GsonFactory.getDefaultInstance(), new InputStreamReader(Objects.requireNonNull(inputStream)));
 
         List<String> scopes = List.of(SheetsScopes.SPREADSHEETS);
 
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                GoogleNetHttpTransport.newTrustedTransport(),
-                GsonFactory.getDefaultInstance(),
-                clientSecrets,
-                scopes
-        ).setDataStoreFactory(new FileDataStoreFactory(new java.io.File("tokens")))
-                .setAccessType("offline")
-                .build();
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(GoogleNetHttpTransport.newTrustedTransport(), GsonFactory.getDefaultInstance(), clientSecrets, scopes).setDataStoreFactory(new FileDataStoreFactory(new java.io.File("tokens"))).setAccessType("offline").build();
 
-        return new AuthorizationCodeInstalledApp(
-                flow,
-                new LocalServerReceiver()
-        ).authorize("user");
+        return new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
     }
 
     public static Sheets getSheetsService() throws IOException, GeneralSecurityException {
         Credential credential = authorize();
-        return new Sheets.Builder(
-                GoogleNetHttpTransport.newTrustedTransport(),
-                GsonFactory.getDefaultInstance(),
-                credential
-        ).setApplicationName(APPLICATION_NAME).build();
+        return new Sheets.Builder(GoogleNetHttpTransport.newTrustedTransport(), GsonFactory.getDefaultInstance(), credential).setApplicationName(APPLICATION_NAME).build();
     }
 
     public static void start() throws GeneralSecurityException, IOException {
+        Professor[] professors = new Professor[31];
+        for (int i = 0; i < professors.length; i++)
+            professors[i] = new Professor();
 
-        for (Professor professor : SheetsAndJava.firstPage()) {
-            System.out.println("Name: " + professor.getName() + " Hours: " + professor.getHours());
+        SheetsAndJava.getNames(professors);
+        SheetsAndJava.getHours(professors, "matrizITI!F4:AJ81", "ITI");
+        SheetsAndJava.getHours(professors, "matrizIM!F4:AJ81", "IM");
+        SheetsAndJava.getHours(professors, "matrizITM!F4:AJ81", "ITM");
+        SheetsAndJava.getHours(professors, "matrizISA!F4:AJ81", "ISA");
+        SheetsAndJava.getHours(professors, "matrizLAyGE!F4:AJ81", "LAyGE");
+        SheetsAndJava.getHours(professors, "matrizComercio!F4:AJ81", "Comercio");
+
+        int totalHours = 0;
+        for (Professor professor : professors) {
+            int tmpTotalHours = (professor.getITIHours() + professor.getIMHours() + professor.getITMHours() + professor.getISAHours() + professor.getLAyGEHours() + professor.getComercioHours());
+            System.out.printf("Name: %20s, ITIHours: %3d, IMHours: %3d, ITMHours: %3d, ISAHours: %3d, LAyGEHours: %3d, comercioHours: %3d, Total hours: %d\n", professor.getName(), professor.getITIHours(), professor.getIMHours(), professor.getITMHours(), professor.getISAHours(), professor.getLAyGEHours(), professor.getComercioHours(), tmpTotalHours);
+
+            totalHours += tmpTotalHours;
+        }
+
+        System.out.println("Total of hours: " + totalHours);
+    }
+
+    public static void getNames(Professor[] professors) throws GeneralSecurityException, IOException {
+        sheetsService = getSheetsService();
+        String namesRange = "matrizITI!F2:AJ2";
+        ValueRange namesResponse = sheetsService.spreadsheets().values().get(SPREADSHEET_ID, namesRange).execute();
+
+        List<List<Object>> names = namesResponse.getValues();
+
+        if (names == null || names.isEmpty()) {
+            System.out.println("No data found");
+            return;
+        }
+
+        for (List<Object> row : names) {
+            for (int i = 0; i < row.size(); i++) {
+                String name = row.get(i).toString();
+                professors[i].setName(name);
+            }
         }
     }
 
-    public static Professor[] firstPage() throws IOException, GeneralSecurityException {
+    public static void getHours(Professor[] professors, String range, String career) throws IOException, GeneralSecurityException {
         sheetsService = getSheetsService();
-        String range = "matrizIM!F4:AJ81";
-//        String namesRange = "matrizITI!F2:AJ2";
         ValueRange response = sheetsService.spreadsheets().values().get(SPREADSHEET_ID, range).execute();
-//        ValueRange namesResponse = sheetsService.spreadsheets().values().get(SPREADSHEET_ID, namesRange).execute();
 
         List<List<Object>> values = response.getValues();
-//        List<List<Object>> names = namesResponse.getValues();
 
-        if ((values == null || values.isEmpty()) /*|| (names == null || names.isEmpty())*/) {
+        if ((values == null || values.isEmpty())) {
             System.out.println("No data found");
-            throw new IOException();
-        }
-
-        Professor[] professors = new Professor[31];
-
-//        for (List<Object> row : names) {
-//            for (int i = 0; i < row.size(); i++) {
-//                String name = row.get(i).toString();
-//                professors[i] = new Professor(name);
-//            }
-//        }
-
-        for (int i = 0; i < professors.length; i++) {
-            professors[i] = new Professor();
+            return;
         }
 
         for (List<Object> row : values) {
             for (int j = 0; j < row.size(); j++) {
                 Object item = row.get(j);
                 int itemInteger = ExceptionHandling.toInt(item);
-                professors[j].setHours(professors[j].getHours() + itemInteger);
+                if (career.equals("ITI")) professors[j].setITIHours(professors[j].getITIHours() + itemInteger);
+                if (career.equals("IM")) professors[j].setIMHours(professors[j].getIMHours() + itemInteger);
+                if (career.equals("ITM")) professors[j].setITMHours(professors[j].getITMHours() + itemInteger);
+                if (career.equals("ISA")) professors[j].setISAHours(professors[j].getISAHours() + itemInteger);
+                if (career.equals("LAyGE")) professors[j].setLAyGEHours(professors[j].getLAyGEHours() + itemInteger);
+                if (career.equals("Comercio")) professors[j].setComercioHours(professors[j].getComercioHours() + itemInteger);
             }
         }
-
-        return professors;
     }
 }
